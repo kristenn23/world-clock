ae9d8a8e0a15fc894a9d45f2e6856faebe8302f7
kristenn23 <nazarethkristen@gmail.com>
2024-06-26 13:06:48 -0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -32,12 +32,13 @@ setInterval(updateIstanbulTime, 1000);
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `  
   <div class= "city">
         <div>
-          <h2>${cityTimeZone}</h2>
+          <h2>${cityName}</h2>
           <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
         <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
