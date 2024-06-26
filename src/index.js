0b6672071166a9224ab7a7ebeba4727ed38d5a83
kristenn23 <nazarethkristen@gmail.com>
2024-06-26 13:04:19 -0400

    added functionality of select element

---
+++ src/index.js
@@ -0,0 +1,49 @@
+//vancouver
+function updateVancouverTime() {
+  let vancouverElement = document.querySelector("#vancouver");
+  if (vancouverElement) {
+    let vancouverDateElement = vancouverElement.querySelector(".date");
+    let vancouverTimeElement = vancouverElement.querySelector(".time");
+    let vancouverTime = moment().tz("America/Vancouver");
+    vancouverDateElement.innerHTML = vancouverTime.format("MMMM Do YYYY");
+    vancouverTimeElement.innerHTML = vancouverTime.format(
+      "h:mm:ss [<small>]A</small>"
+    );
+  }
+}
+updateVancouverTime();
+setInterval(updateVancouverTime, 1000);
+
+//istanbul
+function updateIstanbulTime() {
+  let istanbulElement = document.querySelector("#istanbul");
+  if (istanbulElement) {
+    let istanbulDateElement = istanbulElement.querySelector(".date");
+    let istanbulTimeElement = istanbulElement.querySelector(".time");
+    let istanbulTime = moment().tz("Asia/Istanbul");
+    istanbulDateElement.innerHTML = istanbulTime.format("MMMM Do YYYY");
+    istanbulTimeElement.innerHTML = istanbulTime.format(
+      "h:mm:ss [<small>]A</small>"
+    );
+  }
+}
+updateIstanbulTime();
+setInterval(updateIstanbulTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `  
+  <div class= "city">
+        <div>
+          <h2>${cityTimeZone}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+    "A"
+  )}</small></div>
+      </div>`;
+}
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
