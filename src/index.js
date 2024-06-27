fba094151bfaf0b6bbbe4bcedb19851b04d47935
kristenn23 <nazarethkristen@gmail.com>
2024-06-27 19:40:55 -0400

    added current location

--- src/index.js
+++ src/index.js
@@ -63,7 +63,7 @@ function updateCity(event) {
     "A"
   )}</small></div>
       </div>
-      <a href="index.html> All Cities </a>`;
+      <a href="index.html"> All Cities </a>`;
 }
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
